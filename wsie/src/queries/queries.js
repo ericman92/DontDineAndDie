5682e6b005d8d0db3b69fcd58b71293acf7894e2
Michael Flor <mflor@Michaels-MacBook-Air.local>
2018-03-03 20:11:41 -0500

    added export functions for cuisine, zipcode, dba

--- wsie/src/queries/queries.js
+++ wsie/src/queries/queries.js
@@ -1,8 +1,32 @@
 const axios = require('axios');
 
 const URL = 'https://data.cityofnewyork.us/resource/9w7m-hzhe.json'
+const cuisineURL = `https://data.cityofnewyork.us/resource/9w7m-hzhe.json?$select=count(*),camis,dba,violation_description,street,cuisine_description,boro,zipcode&$group=camis,dba,violation_description,street,cuisine_description,boro,zipcode`
 
-const getTopTen = () => {
-    axios.get(`${URL}$select=count(*)dba&group`)
+export const getCuisine = (cuisine) => {
+    axios.get(cuisineURL+`&cuisine_description=${cuisine}&$limit=20&$order=count(*)DESC`)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => console.log(err))
 }
+
+export const getByZip = (zip) => {
+    axios.get(cuisineURL+`&zipcode=${zip}&$limit=20&$order=count(*)DESC`)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => console.log(err));
+}
+
+export const getByDBA = (name) => {
+    axios.get(cuisineURL+`&dba=${name}&$limit=20&$order=count(*)DESC`)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => console.log(err))
+}
+
+export const getBy
+//https://data.cityofnewyork.us/resource/9w7m-hzhe.json?$select=count(*),camis,dba,violation_description,street,cuisine_description,boro,zipcode&$group=camis,dba,violation_description,street,cuisine_description,boro,zipcode&$WHERE=${cuisine}&$limit=10&$order=count(*)DESC
 
